14d1764e0e1ea29279fff14bfc07d4877aba0e52
Tom de Geus <tom@geus.me>
2018-08-08 11:17:21 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+  name              = 'elasticConstants',
+  version           = '0.0.1',
+  author            = 'Tom de Geus',
+  author_email      = 'tom@geus.me',
+  url               = 'https://github.com/tdegeus/elasticConstants',
+  keywords          = 'elasticity',
+  description       = 'Convert pair of elastic constants',
+  long_description  = '',
+  license           = 'MIT',
+  packages          = ['elasticConstants'],
+)
+
