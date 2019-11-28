7015e12dd6166fb2703b811485b15ccb97cca33c
Tom de Geus <tom@geus.me>
2019-11-28 16:54:43 +0100

    Adding version to module

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 
 from setuptools import setup
+import re
+
+filepath = 'elasticConstants/__init__.py'
+__version__ = re.findall(r'__version__ = \'(.*)\'', open(filepath).read())[0]
 
 setup(
   name = 'elasticConstants',
-  version = '0.0.2',
+  version = __version__,
   author = 'Tom de Geus',
   author_email = 'tom@geus.me',
   url = 'https://github.com/tdegeus/elasticConstants',
